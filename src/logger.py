d0b98b0bd93f449c11ff4c7788e812bcf6cb48c5
abbeymaj <abbey.majumdar@gmail.com>
2025-12-13 18:35:31 -0500

    updated logging file

--- src/logger.py
+++ src/logger.py
@@ -1,1 +1,21 @@
 # Importing packages
+import logging
+import os
+from datetime import datetime
+
+# Creating a format for the log file
+LOG_FILE = f"{datetime.now().strftime('%m_%d_%Y_%H_%M_%S')}.log"
+
+# Creating the directory where the log file will be stored
+logs_path = os.path.join(os.getcwd(), "logs")
+os.makedirs(logs_path, exist_ok=True)
+
+# Creating the path to the logs file
+LOG_FILE_PATH = os.path.join(logs_path, LOG_FILE)
+
+# Overriding the default behaviour of the logging module
+logging.basicConfig(
+    filename=LOG_FILE_PATH,
+    format="[ %(asctime)s ] %(lineno)d %(name)s - %(levelname)s - %(message)s",
+    level=logging.INFO
+)
